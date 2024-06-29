265a21b2ed1509d701c323e2e80e8fa76541dd5d
tasib94 <rahi84974@gmail.com>
2024-06-29 13:16:50 -0600

    blogs are updating

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Nav from '../Components/Nav';
 import Hero from '../Components/Hero';
+import Blogs from '../Components/Blogs';
 
 const Home = () => {
     return (
@@ -10,6 +11,12 @@ const Home = () => {
            <div className='w-3/4 mx-auto mt-16'>
            <Hero />
            </div>
+           <div className='mt-20'>
+            <h3 className='font-bold text-4xl text-center mb-12'>Books</h3>
+
+            <Blogs />
+           </div>
+           
         </div>
     );
 };
